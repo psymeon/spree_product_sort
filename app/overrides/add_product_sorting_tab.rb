ded2cf03b93c36abb4af4a5ab47e9548b7cd6a33
Patrick McElwee <pmcelwee@gmail.com>
2012-05-25 23:17:58 -0400

    Initial changes for 0.70

---
+++ app/overrides/add_product_sorting_tab.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => 'admin/shared/_product_sub_menu',
+		     :name => 'add_product_sorting_tab',
+		     :insert_bottom => "[data-hook='admin_product_sub_tabs']",
+		     :text => %q{<%= tab :product_sorting, :route => 'positions_admin_product_taxons' %>}
+)
--- lib/spree_product_sort.rb
+++ lib/spree_product_sort.rb
@@ -1,14 +1,16 @@
 require 'spree_core'
-require 'spree_product_sort_hooks'
 
 module SpreeProductSort
   class Engine < Rails::Engine
-
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/**/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
 
