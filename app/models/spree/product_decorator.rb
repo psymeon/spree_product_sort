4396db4902884e77dbf7cdd0bd56414eb889f5b9
Patrick McElwee <pmcelwee@gmail.com>
2012-05-29 22:33:29 -0400

    Namespaced odd ProductTaxon model

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,13 +2,13 @@ Spree::Product.class_eval do
   has_many :product_taxons
   has_many :taxons, :through=>:product_taxons
 
-  #default_scope :include=>:product_taxons, :order=>"product_taxons.position"
-  scope :ordered, {:include=>:product_taxons, :order=>"product_taxons.position"}
+  #default_scope :include=>:product_taxons, :order=>"spree_product_taxons.position"
+  scope :ordered, {:include=>:product_taxons, :order=>"#{Spree::ProductTaxon.quoted_table_name}.position"}
 
   scope :available, lambda { |*args| 
-    where(["products.available_on <= ?", args.first || Time.zone.now]).
+    where(["#{Product.quoted_table_name}.available_on <= ?", args.first || Time.zone.now]).
       includes(:product_taxons).
-      order('product_taxons.taxon_id, product_taxons.position') #group positions by taxon so that home page (0) works
+      order("#{Spree::ProductTaxon.quoted_table_name}.taxon_id, #{Spree::ProductTaxon.quoted_table_name}.position") #group positions by taxon so that home page (0) works
   }
 
   def in_taxon?(taxon)
