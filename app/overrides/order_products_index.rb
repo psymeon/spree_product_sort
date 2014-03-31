51ef7acb61d638e0a0aa02c9e63f952d8022ff2e
Patrick McElwee <pmm21@duke.edu>
2014-03-30 22:32:47 -0400

    Support Spree 1.2

--- app/overrides/order_products_index.rb
+++ app/overrides/order_products_index.rb
@@ -1,14 +1,15 @@
 Deface::Override.new(:virtual_path => 'spree/products/index',
 		     :name => 'order_products_index',
 		     :replace_contents => "[data-hook='homepage_products']",
-		     :text => %q{<%  if @taxon %>
-      <%    tid = @taxon.id %>
-    <%  else %>
-      <%    tid = 0 %>
-      <%- #covers special case of main index %>
-    <%  end %>
+		     :text => %q{
+                     <% if @taxon %>
+                       <% tid = @taxon.id %>
+                     <% else %>
+                       <% tid = 0 %>
+                     <% end %>
 
-    <%= render :partial => 'spree/shared/products', :locals => { :products => @products.ordered(tid), :taxon => @taxon } %>},
+                     <%= render :partial => 'spree/shared/products', :locals => { :products => @products.ordered(tid), :taxon => @taxon } %>},
+
     		     :original => %q{<div data-hook="homepage_products">
     <%= render :partial => 'spree/shared/products', :locals => { :products => @products, :taxon => @taxon } %>
   </div>}
